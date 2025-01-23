3ad6727cba1ad9e5302170a4dad06d7b509a61ae
Stela <stelaelezi52@gmail.com>
2025-01-23 18:24:51 +0100

    Built the carousel section

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
 const products = document.querySelectorAll(".product");
 const carousel = document.querySelector(".carousel");
-const nextBtn = document.querySelector(".next-button");
-const prevBtn = document.querySelector(".prev-button");
 let currentIndex = 0;
+const totalSlides = products.length;
 
-function showSlide() {
+const getItemsPerView = (width) => {
+  return width > 1000 ? 3 : width > 650 ? 2 : 1;
+};
+
+let itemsPerView = getItemsPerView(window.innerWidth);
+
+const updateSlides = () => {
   products.forEach((product) => {
     product.style.display = "none";
   });
 
-  products[index].style.display = "block";
-}
+  for (let i = 0; i < itemsPerView; i++) {
+    products[currentIndex + i].style.display = "block ";
+  }
+};
+
+const nextSlide = () => {
+  if (currentIndex + itemsPerView === totalSlides - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  updateSlides();
+};
+
+const prevSlide = () => {
+  console.log("prev");
+  if (currentIndex === 0) {
+    currentIndex = totalSlides - itemsPerView;
+  } else {
+    currentIndex--;
+  }
+  updateSlides();
+};
+
+const nextBtn = document
+  .querySelector(".next-button")
+  .addEventListener("click", () => {
+    nextSlide();
+  });
+
+const prevBtn = document
+  .querySelector(".prev-button")
+  .addEventListener("click", () => {
+    prevSlide();
+  });
+
+window.addEventListener("resize", () => {
+  itemsPerView = getItemsPerView(window.innerWidth);
+  //change slides once the screen size has changed
+  updateSlides();
+});
